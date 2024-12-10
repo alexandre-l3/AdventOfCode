ca4499a7c609960871686f5b4246fb0021fbfef1
ale <d.alexandre.le@gmail.com>
2024-12-10 16:11:27 +1000

    Solve day 10

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -3,7 +3,7 @@
 using System.Diagnostics;
 using AdventOfCode;
 
-var day = new Day9();
+var day = new Day10();
 var startTimePart1 = Stopwatch.GetTimestamp();
 Console.WriteLine(day.SolvePartOne());
 Console.WriteLine($"Time taken for part 1: {Stopwatch.GetElapsedTime(startTimePart1)}");
