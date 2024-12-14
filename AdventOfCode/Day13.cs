d6e1b8245ff617e2ed99d9a9342ea54778f0b6b8
ale <d.alexandre.le@gmail.com>
2024-12-14 13:23:03 +1000

    Clean up day 13

--- AdventOfCode/Day13.cs
+++ AdventOfCode/Day13.cs
@@ -2,107 +2,57 @@ using System.Text.RegularExpressions;
 
 namespace AdventOfCode;
 
-public sealed class Day13 : IDay
+public sealed partial class Day13 : IDay
 {
-    public string SolvePartOne()
-    {
-        var pattern = "\\d+";
-
-        var result = 0L;
-        var line = 0;
-        var pairs = new List<Pair>();
-        foreach (var input in _inputs)
-        {
-            var matches = Regex.Matches(input, pattern)
-                .Select(m => int.Parse(m.Value))
-                .ToArray();
-            pairs.Add(new Pair(matches[0], matches[1]));
-            line++;
-            if (line == 3)
-            {
-                result += CalculateCost(pairs[0], pairs[1], pairs[2]);
-                line = 0;
-                pairs.Clear();
-            }
-        }
-
-        return result.ToString();
-    }
-
-    public string SolvePartTwo()
-    {
-        var pattern = "\\d+";
-
-        var result = 0L;
-        var line = 0;
-        var pairs = new List<Pair>();
-        foreach (var input in _inputs)
-        {
-            var matches = Regex.Matches(input, pattern)
-                .Select(m => int.Parse(m.Value))
-                .ToArray();
-            pairs.Add(new Pair(matches[0], matches[1]));
-            line++;
-            if (line == 3)
-            {
-                var correctedTarget = new Pair(CorrectiveTerm + pairs[2].X, CorrectiveTerm + pairs[2].Y);
-                result += CalculateCostV2(pairs[0], pairs[1], correctedTarget);
-                line = 0;
-                pairs.Clear();
-            }
-        }
-
-        return result.ToString();
-    }
-
     private const long CorrectiveTerm = 10000000000000L;
 
-    private sealed record Pair(long X, long Y);
+    [GeneratedRegex("\\d+")]
+    private static partial Regex NumberRegex();
+
+    public string SolvePartOne() => Solve(CalculateCost).ToString();
+
+    public string SolvePartTwo() => Solve((a, b, target) =>
+        {
+            var correctedTarget = new Pair(CorrectiveTerm + target.X, CorrectiveTerm + target.Y);
+            return CalculateCost(a, b, correctedTarget, _ => true);
+        }).ToString();
+
+    private long Solve(Func<Pair, Pair, Pair, long> costCalculator) => _inputs
+        .Select(input => NumberRegex().Matches(input).Select(m => int.Parse(m.Value)).ToArray())
+        .Select(matches => new Pair(matches[0], matches[1]))
+        .Chunk(3)
+        .Sum(chunk => costCalculator(chunk[0], chunk[1], chunk[2]));
 
     private static long CalculateCost(Pair a, Pair b, Pair target)
     {
-        var determinant = a.X * b.Y - b.X * a.Y;
-        if (determinant == 0)
-        {
-            return 0L;
-        }
-
-        var aPresses = b.Y * target.X - b.X * target.Y;
-        var bPresses = -a.Y * target.X + a.X * target.Y;
-        if (aPresses % determinant == 0 && bPresses % determinant == 0)
-        {
-            aPresses /= determinant;
-            bPresses /= determinant;
-
-            if (aPresses is > 0 and < 100 && bPresses is > 0 and < 100)
-            {
-                return 3 * aPresses + bPresses;
-            }
-        }
-
-        return 0L;
+        return CalculateCost(a, b, target, p => p.aPresses is > 0 and < 100 && p.bPresses is > 0 and < 100);
     }
 
-    private static long CalculateCostV2(Pair a, Pair b, Pair target)
+    private static long CalculateCost(Pair a, Pair b, Pair target, Func<(long aPresses, long bPresses), bool> validate)
     {
         var determinant = a.X * b.Y - b.X * a.Y;
-        if (determinant == 0)
+        if (determinant == 0L)
         {
             return 0L;
         }
 
         var aPresses = b.Y * target.X - b.X * target.Y;
         var bPresses = -a.Y * target.X + a.X * target.Y;
-        if (aPresses % determinant == 0 && bPresses % determinant == 0)
+        if (aPresses % determinant != 0 || bPresses % determinant != 0)
         {
-            aPresses /= determinant;
-            bPresses /= determinant;
-
+            return 0L;
+        }
+        aPresses /= determinant;
+        bPresses /= determinant;
+        if (validate((aPresses, bPresses)))
+        {
             return 3 * aPresses + bPresses;
         }
 
         return 0L;
     }
+
+    private sealed record Pair(long X, long Y);
 
     private readonly string[] _inputs =
     [
